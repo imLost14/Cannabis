53bacf6a9ce74c25062c0eb33b97fe53956894e4
HectorDiaz <hrdc.1407@hotmail.com>
2023-04-09 14:58:09 -0500

    agregue caracteres y etiquetas a la grafica de barras

--- charts.py
+++ charts.py
@@ -11,5 +11,8 @@ def generate_pie_chart(labels, values):
 
 def generate_bar_chart(labels, values):
   fig, ax = plt.subplots()
+  ax.set_ylabel('Raiting', color='black')
+  ax.set_xlabel('Tipo de cannabis', color ='green', size =20)
   ax.bar(labels, values,width = 0.8, bottom = 0.1, edgecolor = "black", color = 'gray')
+  ax.set_title('Raiting de tipo de Cannabis')
   plt.show()
