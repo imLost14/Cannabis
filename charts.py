44806a417be276292dfa223e6fd1e87a3a7c098e
HectorDiaz <hrdc.1407@hotmail.com>
2023-04-09 23:37:55 -0500

    no estaba en pie

--- charts.py
+++ charts.py
@@ -7,7 +7,8 @@ def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels= labels, autopct='%.1f%%', colors = colores, shadow = True, startangle=90)
   ax.axis('equal')
-  plt.show()
+  plt.savefig('pie.png')
+  plt.close()
 
 
 def generate_bar_chart(labels, values):
@@ -16,8 +17,7 @@ def generate_bar_chart(labels, values):
   ax.set_xlabel('Tipo de cannabis', color ='green', size =20)
   ax.bar(labels, values,width = 0.8, bottom = 0.1, edgecolor = "black", color = 'gray')
   ax.set_title('Raiting de tipo de Cannabis')
-  plt.savefig('pie.png')
-  plt.close
+  
   
 
   
